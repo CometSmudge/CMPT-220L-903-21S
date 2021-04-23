241417937096eb102592d24399a0db7c29f49494
CometSmudge <70175242+CometSmudge@users.noreply.github.com>
2021-04-23 13:57:29 -0400

    did something

--- Assignments/src/com/company/Main.java
+++ Assignments/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] bubblelist = {7, 3, 9, 5};
+        int a = 0;
+        int b = 0;
+        for (int x : bubblelist) {
+            if(bubblelist[x] > bubblelist[x + 1]){
+                a = bubblelist[x];
+                b = bubblelist[x+1];
+                bubblelist[x] = b;
+                bubblelist[x + 1] = a;
+            }
+        }
+
+
     }
 }
